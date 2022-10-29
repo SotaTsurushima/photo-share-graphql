4b2673c0e52752eaf8471c396ee4e87dfb75f4bb
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-10-29 17:18:10 +0900

    add 型定義

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
+
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `
 
+// ユニークIDをインクリメントするための変数
+let id = 0
+let photos = []
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+
+      // 新しい写真を作成し、idを生成する
+      let newPhoto = {
+        id: id++,
+        ...args
+      }
+      photos.push(newPhoto)
+
+      // 新しい写真を返す
+      return newPhoto
+    }
+  },
+  Photo: {
+    url: parent => `htto://yoursite.com/img/${parent.id}.jpg`
   }
 }
 
