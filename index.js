7579ea4f0c07199d8fcc7ed41d09a0f7f8932db2
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-11-04 19:27:20 +0900

    DateTime型の追加

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
+  scalar DateTime
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -17,11 +19,12 @@ const typeDefs = `
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
+    created: DateTime!
   }
 
   type Query {
     totalPhotos: Int!
-    allPhotos: [Photo!]!
+    allPhotos(after: DateTime): [Photo!]!
   }
 
   input PostPhotoInput {
@@ -73,6 +76,13 @@ let photos = [
   },
 ]
 
+let tags = [
+  { "photoID": "1", "userID": "gPlake" },
+  { "photoID": "2", "userID": "sSchmidt" },
+  { "photoID": "2", "userID": "mHattrup" },
+  { "photoID": "2", "userID": "gPlake" }
+]
+
 // ユニークIDをインクリメントするための変数
 let id = 0
 // let photos = []
@@ -98,12 +108,30 @@ const resolvers = {
     url: parent => `htto://yoursite.com/img/${parent.id}.jpg`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
-    }
+    },
+    taggedUsers: parent => tags
+      // 対象の写真が関係しているタグの配列を返す
+      .filter(tag => tag.photoID === parent.id)
+
+      // タグの配列をユーザーIDの配列に変換する
+      .map(tag => tag.userID)
+
+      // ユーザーIDの配列をユーザーオブジェクトの配列に変換する
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+      // 対象のユーザーが関係しているタグの配列を返す
+      .filter(tag => tag.userID === parent.id)
+
+      // タグの配列を写真IDの配列に変換する
+      .map(tag => tag.photoID)
+
+      // 写真IDの配列を写真オブジェクトの配列に変換する
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
